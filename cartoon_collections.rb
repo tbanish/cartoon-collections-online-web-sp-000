71b37f67e228494de034220913efc12c48494ebd
Tom Banish <tombanish@gmail.com>
2019-11-27 02:08:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,14 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  num_dwarves = []
+  array.each_with_index do |name, index|
+    puts "#{index.to_i + 1} #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map {|call| "#{call.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+
+
+def long_planeteer_calls(the_calls)
+  the_calls.map do |call|
+
+
+
+
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
